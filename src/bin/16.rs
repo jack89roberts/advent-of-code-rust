621727ec0bd063ed681e3448451fde9bfe4b9859
jack89roberts <jroberts@turing.ac.uk>
2022-12-16 22:39:34 +0000

    p2 v2

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -112,47 +112,44 @@ fn run_p2(
     times: Vec<u32>,           // current time for each agent
     positions: Vec<usize>,     // most recently visited valve for each agent
     end: u32,                  // end time
-    open_valves: Vec<usize>,   // valves that have been turned on so far
+    remaining_valves: Vec<usize>,   // valves that have NOT been turned on so far
     current_pressure: u32, // if no more valves were turned on, total pressure achieved at end time
     mut best_pressure: u32, // best total pressure achieved so far
 ) -> u32 {
     // candidate valve to visit next
-    for valve in 0..flows.len() {
-        // have we already opened this valve?
-        if !open_valves.contains(&valve) {
-            for agent_idx in 0..positions.len() {
-                // time taken to open valve is distance to it +1
-                let new_t = times[agent_idx] + distances[positions[agent_idx]][valve] + 1;
+    for valve in &remaining_valves {
+        for agent_idx in 0..positions.len() {
+            // time taken to open valve is distance to it +1
+            let new_t = times[agent_idx] + distances[positions[agent_idx]][*valve] + 1;
 
-                // would opening this valve take us over the time limit?
-                if new_t < end {
-                    // add pressure released in remaining time from opening this valve, and check if this is a new best
-                    let new_pressure = current_pressure + (end - new_t) * flows[valve];
-                    let new_best = if new_pressure > best_pressure {
-                        new_pressure
-                    } else {
-                        best_pressure
-                    };
+            // would opening this valve take us over the time limit?
+            if new_t < end {
+                // add pressure released in remaining time from opening this valve, and check if this is a new best
+                let new_pressure = current_pressure + (end - new_t) * flows[*valve];
+                let new_best = if new_pressure > best_pressure {
+                    new_pressure
+                } else {
+                    best_pressure
+                };
 
-                    // run again with updated values (after adding new valve to the list of opened ones)
-                    let mut new_valves = open_valves.clone();
-                    new_valves.push(valve);
-                    let mut new_positions = positions.clone();
-                    new_positions[agent_idx] = valve;
-                    let mut new_times = times.clone();
-                    new_times[agent_idx] = new_t;
+                // run again with updated values (after adding new valve to the list of opened ones)
+                let mut new_valves = remaining_valves.clone();
+                new_valves.retain(|v| *v != *valve);
+                let mut new_positions = positions.clone();
+                new_positions[agent_idx] = *valve;
+                let mut new_times = times.clone();
+                new_times[agent_idx] = new_t;
 
-                    best_pressure = run_p2(
-                        flows,
-                        distances,
-                        new_times,
-                        new_positions,
-                        end,
-                        new_valves,
-                        new_pressure,
-                        new_best,
-                    );
-                }
+                best_pressure = run_p2(
+                    flows,
+                    distances,
+                    new_times,
+                    new_positions,
+                    end,
+                    new_valves.to_vec(),
+                    new_pressure,
+                    new_best,
+                );
             }
         }
     }
@@ -181,13 +178,17 @@ pub fn part_two(input: &str) -> Option<u32> {
         .collect_vec();
     let start_idx = good_valves.iter().position(|v| v == "AA").unwrap();
 
+    // possible next valves (remaining) = all valves except the start one AA, whih always has 0 flow
+    let mut remaining_valves = (0..flows.len()).collect_vec();
+    remaining_valves.retain(|v| *v != start_idx);
+
     let best_total = run_p2(
         &flows,
         &distances,
         vec![0, 0],
         vec![start_idx, start_idx],
         26,
-        vec![start_idx],
+        remaining_valves,
         0,
         0,
     );
